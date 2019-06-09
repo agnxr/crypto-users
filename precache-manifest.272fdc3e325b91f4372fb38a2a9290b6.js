c3b3cc4fc292c50dd77a332a1b8477e1c7c188bb
agnxr <ar21@o2.pl>
2019-06-10 00:24:03 +0200

    Updates

---
+++ precache-manifest.272fdc3e325b91f4372fb38a2a9290b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4180749662c3889745b340e86e1ca03d",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "46d71c55df4792096525",
+    "url": "/user-list/static/css/main.6f001e08.chunk.css"
+  },
+  {
+    "revision": "d9f24fa283cd9ce78112",
+    "url": "/user-list/static/js/2.84ccf56d.chunk.js"
+  },
+  {
+    "revision": "46d71c55df4792096525",
+    "url": "/user-list/static/js/main.3c135baa.chunk.js"
+  },
+  {
+    "revision": "5daf1e6574934a7d0a9b",
+    "url": "/user-list/static/js/runtime~main.b6747a5c.js"
+  }
+]);
